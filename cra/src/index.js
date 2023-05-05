e57cd14709faf60db2cdb7bbde3378ff505f1b8c
himanshukumar30 <himanshuk.kumar01@gmail.com>
2023-05-05 12:40:41 -0700

    adding components to pick rendom fruit and log it

--- cra/src/index.js
+++ cra/src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import { choice, remove } from "./helpers";
+import Fruits from "./Foods";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let fruit = choice(Fruits);
+console.log(`I’d like one ${fruit}, please.`);
+console.log(`Here you go: ${fruit}`);
+console.log(`Delicious! May I have another?`);
+let remaining = remove(Fruits, `${fruit}`);
+console.log(`I'm sorry, we're all out. We have ${remaining} left`);
